e255ee176857e7d76890e9bec3b11f949a51ec87
ShreyashBRN <shreyashbrn@gmail.com>
2026-06-30 01:08:54 +0530

    fix github actions version

--- keywords.js
+++ keywords.js
@@ -7,15 +7,3 @@ module.exports = [
     "web developer",
     "website developer"
   ];
-
-  module.exports = [
-    "freelance_forhire",
-    "forhire",
-    "developers_hire",
-    "hiring",
-    "B2BForHire",
-    "FullStackDevelopers",
-    "ForHireFreelance",
-    "WebDeveloperJobs",
-    "devjobs"
-  ];
---
+++ subreddits.js
@@ -0,0 +1,11 @@
+module.exports = [
+    "freelance_forhire",
+    "forhire",
+    "developers_hire",
+    "hiring",
+    "B2BForHire",
+    "FullStackDevelopers",
+    "ForHireFreelance",
+    "WebDeveloperJobs",
+    "devjobs"
+  ];
